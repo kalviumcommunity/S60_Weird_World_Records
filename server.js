b4a5f8d4844275b85b321e50bbadf2d19a84a400
Poshika <poshika@Poshikas-MacBook-Air.local>
2024-04-05 12:32:57 +0530

    worked with mongo atlas and added client code

---
+++ server.js
@@ -0,0 +1,20 @@
+const {modelVar, connectVar} = require('./mongo')
+const express = require('express')
+const server = express();
+
+console.log('Checking')
+
+function checkStatus(){
+    return modelVar.db.readyState === 1;
+}
+
+server.get('/', (request, response) => {
+    const connectStatus = checkStatus()
+    let verifyConnc = connectStatus ? 'Connected Successfully' : 'Not connected' 
+    response.send(verifyConnc)
+})
+
+server.listen(3001, () => {
+    connectVar()
+    console.log('Created a server.js file, coded in it and pushing it')
+})
