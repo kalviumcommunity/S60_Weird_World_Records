5df42e9562c71a92c69047dc6a6ffb80820f4544
Poshika <poshika@Poshikas-MacBook-Air.local>
2024-04-08 16:28:50 +0530

    added a single dummy data of my ASAP project

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import data from './Components/dummydata'
 
 function App() {
 
   return (
     <div>
-      <h3>Created a landing page about my ASAP idea</h3>
+      <p>Record Category : {data.Record_category}</p>
+      <p>Record Name : {data.Record_Name}</p>
+      <p>Record Holder Name : {data.Record_Holder_Name}</p>
+      <img src={data.Record_Picture} alt="" />
+      <p>Record  Details : {data.Record_Details}</p>
     </div>
   )
 }
